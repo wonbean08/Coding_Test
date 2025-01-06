eea507d692fa7221b9cc5b46677b129a40a438a7
wonbean08 <wonbean08@naver.com>
2025-01-06 23:21:06 +0900

    [level 0] Title: 홀짝 구분하기, Time: 350.46 ms, Memory: 62.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/181944. 홀짝 구분하기/홀짝 구분하기.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Solution {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        Check(n);
+    }
+    public static void Check(int n){
+        if(n%2==1) 
+            System.out.println(n+" is odd");
+        else
+            System.out.println(n+" is even");
+    }
+}
