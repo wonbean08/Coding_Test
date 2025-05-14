8e2a3fadb16e0341d6f45b5c72b784f9c89daba4
wonbean08 <wonbean08@naver.com>
2025-05-14 18:07:35 +0900

    [level 2] Title: 숫자의 표현, Time: 2.50 ms, Memory: 36.2 MB -BaekjoonHub

---
+++ 프로그래머스/2/12924. 숫자의 표현/숫자의 표현.js
@@ -0,0 +1,17 @@
+function solution(n) {
+    let answer = 0;
+    for(let i=1; i<=n; i++){
+        let sum=0;
+        for(let j=i; j<=n; j++){
+            sum+=j;
+            if(sum===n){
+                answer++;
+                break;
+            }
+            if(sum>n){
+                break;
+            }
+        }
+    }
+    return answer;
+}
